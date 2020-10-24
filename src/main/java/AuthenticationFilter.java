81e04f32a4b4f51e70110daaada9ed95ff8ef54b
fabio <fabio.meihua08@gmail.com>
2020-10-24 15:14:57 -0300

    First Commit

---
+++ src/main/java/AuthenticationFilter.java
@@ -0,0 +1,12 @@
+public class AuthenticationFilter implements Filter{
+
+    public void execute ( String request){
+
+        System.out.println("Authentication request : " + request);
+
+    }
+
+
+
+
+}
